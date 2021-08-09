cbda3859395a617d05543f2130763d20a3f7bad8
Julio Barrios <julio.junior02@gmail.com>
2021-08-09 18:08:12 -0500

    Add description

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -3,6 +3,7 @@ import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state"
 import ShareIcon from "@material-ui/icons/Share";
 import { FC } from "react";
 
+// Component to extra info like a url to share
 const Popover: FC<{pathName: string, handle: string}> = ({pathName, handle}) => {
     return (
         <PopupState variant="popover">
@@ -24,7 +25,7 @@ const Popover: FC<{pathName: string, handle: string}> = ({pathName, handle}) => 
                     >
                         <Box p={2}>
                           <Typography variant="caption">Copy url to share</Typography>
-                          <Typography>{`${window.location.hostname}/companies/${handle}`}</Typography>
+                          <Typography>{`${window.location.hostname}/${pathName}/${handle}`}</Typography>
                         </Box>
                     </PopoverMU>
                 </>
