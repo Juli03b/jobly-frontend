efcb268cecd225dc7b8d084e85b9443f78bc1e09
Julio Barrios <julio.junior02@gmail.com>
2021-08-09 17:57:01 -0500

    Add constants for form defaults

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,22 @@
+import { UserCreateProps, UserPatchProps, UserSignInProps } from "./interfaces";
+
 export const NO_USER_MSG = "Login or sign up to view jobs and companies";
 export const IMG_NOT_FOUND = "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg"
+export const BACKGROUND_IMG = "https://digitaladdictsblog.com/wp-content/uploads/2018/01/Digital-Addicts-superior-spy-apps-catch-a-cheater-smartphone.jpg";
+export const SIGN_IN_INITIAL_STATE: UserSignInProps = {
+    username: "",
+    password: ""
+}
+export const SIGN_UP_INITIAL_STATE: UserCreateProps = {
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+}
+export const PATCH_INITIAL_STATE: UserPatchProps = {
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+}
