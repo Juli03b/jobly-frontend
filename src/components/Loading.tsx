2c70e294ada78ed8a90c1f50c073ff7d4075692d
Julio Barrios <julio.junior02@gmail.com>
2021-08-09 18:07:30 -0500

    Add description

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,9 @@
 import { CircularProgress, Typography } from "@material-ui/core"
 
-export const Loading = () => {
-    return (
+// Dumb component to show loading gif
+export const Loading = () => (
         <>
             <CircularProgress style={{width:"25%", height: "-1px", marginTop: "15%", marginLeft: "40%"}} />
             <Typography variant="h1" style={{width:"25%", height: "-1px", marginTop: "15%", marginLeft: "40%"}}>Loading...</Typography>
         </>
-    );
-}
+);
